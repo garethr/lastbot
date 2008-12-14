8610fb6d2bf9bb958bf2dc0d698fa21c138c1935
Gareth Rushgrove <gareth@morethanseven.net>
2008-12-14 13:41:13 +0000

    working local search bot

---
+++ lastsearchbot.py
@@ -0,0 +1,114 @@
+#!/usr/bin/python
+
+"""
+A jabber bot that sits and waits for you to ask it questions
+then goes off to Last.fm to get some information about the track
+you mentioned. Requires the pylast library that can be found at
+http://code.google.com/p/pylast
+
+Also requires a settings file with the following constants defined:
+
+USERNAME = ""
+PASSWORD = ""
+API_KEY = ""
+API_SECRET = ""
+SESSION_KEY = ""
+
+"""
+
+import sys
+import re
+import htmllib
+from xml.dom import minidom
+
+import pylast
+from jabberbot import JabberBot
+
+import settings
+
+def unescape(s):
+    "Helper to unescape html entities and other encoding"
+    p = htmllib.HTMLParser(None)
+    p.save_bgn()
+    p.feed(s)
+    return p.save_end()
+
+class LastFmJabberBot(JabberBot):
+    """Personal LastFM search bot. Useful for looking for tracks you can't 
+remember the name of."""
+
+    def _display_track_details(self):
+        "Display details about the track"
+        try:
+            # get any info we can from the API
+            title = self.results[self.pointer].getTitle()
+            url = self.results[self.pointer].getURL()
+            artist = self.results[self.pointer].getArtist()
+            artist_bio = artist.getBioSummary()
+            artist_name = artist.getName()
+
+            if self.pointer == 0:
+                intro = "you probably mean"
+            else:
+                intro = "you might have meant"
+
+            return """
+%s %s by %s
+
+%s
+""" % (intro, title, artist_name, unescape(re.sub(r'<[^>]*?>', '', artist_bio)))
+        except:
+            return "We didn't find anything!"    
+
+    def bot_next(self, mess, args):
+        "get details about the next track in the list"
+        try:
+            self.pointer = self.pointer + 1
+            return self._display_track_details()
+        except AttributeError:
+            return "You have to search for something first"
+
+    def bot_prev(self, mess, args):
+        "get details about the previous track in the list"
+        try:
+            self.pointer = self.pointer - 1
+            return self._display_track_details()
+        except AttributeError:
+            return "You have to search for something first"
+
+    def bot_search(self, mess, args):
+        "do a search for tracks"
+        # it's a new search so lets reset the pointer
+        self.pointer = 0
+        
+        try:
+            # parse the message
+            dom = minidom.parseString(str(mess))
+            # get the first body element
+            body = dom.getElementsByTagName('body')[0]
+            # grab the data after the search command
+            search_for = body.childNodes[0].data[7:]
+        except:
+            # something went horribly wrong
+            return "A problem occured"
+            sys.exit(2)
+        
+        if not search_for:
+            return "You have to search for something"
+        
+        # we can now do a search
+        search = pylast.TrackSearch(search_for, None, settings.API_KEY, \
+            settings.API_SECRET, settings.SESSION_KEY)
+        # and get the results
+        self.results = search.getResults()
+        
+        # and then display the results back to the sender
+        return self._display_track_details()
+
+def main():
+    "Connect to the server and run the bot forever"
+    jabber_bot = LastFmJabberBot(settings.USERNAME, settings.PASSWORD)
+    jabber_bot.serve_forever()
+
+if __name__ == '__main__':    
+    main()
